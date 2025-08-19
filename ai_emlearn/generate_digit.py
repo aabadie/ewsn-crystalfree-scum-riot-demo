f078750efce39128d121a829159c3e4298c9d787
Alexandre Abadie <alexandre.abadie@inria.fr>
2025-08-19 08:04:45 +0200

    ai_emlearn: send digit over serial and do interence in a loop

--- ai_emlearn/generate_digit.py
+++ ai_emlearn/generate_digit.py
@@ -4,40 +4,44 @@
 Pixel of the sample are stored as float32, images have size 8x8.
 """
 
-import os
 import argparse
+import struct
 
-import numpy as np
 import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
 
-SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+import serial
+
+SERIAL_PORT = '/dev/ttyUSB0'
+SERIAL_BAUDRATE = 19200
 
 
 def main(args):
-    output_path = os.path.join(SCRIPT_DIR, args.output)
     digits = datasets.load_digits()
 
     rnd = 42
-    _, data, _, _ = train_test_split(digits.data, digits.target,
-                                     random_state=rnd)
+    _, data, _, _ = train_test_split(digits.data, digits.target, random_state=rnd)
     data = data[args.index]
-    np.ndarray.tofile(data.astype('float32'), output_path)
 
-    if args.no_plot is False:
-        plt.gray()
-        plt.imshow(data.reshape(8, 8))
-        plt.show()
+    plt.gray()
+    plt.imshow(data.reshape(8, 8))
+
+    ser = serial.Serial(SERIAL_PORT, SERIAL_BAUDRATE)
+    for pixel in data.astype('float32'):
+        for byte_ in bytearray(struct.pack("f", pixel)):
+            ser.write(byte_)
+    ser.flush()
+    ser.close()
+    plt.show()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--index", type=int, default=0,
                         help="Image index in MNIST test dataset")
-    parser.add_argument("-o", "--output", type=str, default='digit',
-                        help="Output filename")
-    parser.add_argument("--no-plot", default=False, action='store_true',
-                        help="Disable image display in matplotlib")
-    main(parser.parse_args())
+    try:
+        main(parser.parse_args())
+    except Exception as e:
+        print(f"Error: {e}")
--- ai_emlearn/main.c
+++ ai_emlearn/main.c
@@ -2,35 +2,52 @@
  * Copyright (C) 2019 Inria
  */
 
-/**
- * @ingroup     tests
- * @{
- *
- * @file
- * @brief       Emlearn test application
- *
- * @author      Alexandre Abadie <alexandre.abadie@inria.fr>
- *
- * @}
- */
-
 #include <inttypes.h>
 #include <stdalign.h>
 #include <stdio.h>
 
+#include "board.h"
+#include "stdio_uart.h"
+#include "thread.h"
+#include "periph/uart.h"
+
 #include "model.h"
 
-/* the digit array will be casted to float, so it has to meet that alignment */
-alignas(float)
-#include "blob/digit.h"
+#define IMG_SIZE 256U
+
+static alignas(float) uint8_t _digit[IMG_SIZE] = { 0 };
+static uint8_t _digit_pos = 0;
+
+static kernel_pid_t _main_pid;
+
+static void _uart_callback(void *arg, uint8_t data) {
+    (void)arg;
+    msg_t msg;
+    msg.content.value = (uint32_t)data;
+    msg_send(&msg, _main_pid);
+}
 
 int main(void)
 {
-    /* Use intermediate cast to uintptr_t to silence -Wcast-align. Since
-     * we add the alignas() attribute, the alignment is ensured */
-    const float *digit_as_float = (const float *)(uintptr_t)digit;
-    printf("Predicted digit: %" PRIi32 "\n",
-           model_predict(digit_as_float, digit_len >> 2));
+    _main_pid = thread_getpid();
+
+    /* Initialize first UART */
+    uart_init(UART_DEV(0), STDIO_UART_BAUDRATE, _uart_callback, NULL);
+
+    /* Wait for UART data */
+    msg_t msg;
+    msg_t msg_queue[8];
+    msg_init_queue(msg_queue, 8);
+
+    while (1) {
+        msg_receive(&msg);
+        _digit[_digit_pos++] = (uint8_t)msg.content.value;
+        if (_digit_pos == 0) {
+            const float *digit_as_float = (const float *)(uintptr_t)_digit;
+            printf("Predicted digit: %" PRIi32 "\n",
+                   model_predict(digit_as_float, IMG_SIZE >> 2));
+        }
+    }
 
     return 0;
 }
